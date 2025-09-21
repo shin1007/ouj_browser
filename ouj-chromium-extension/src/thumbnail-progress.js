8de625f334729b752760338716d488b9a6a2eff1
Shinnosuke <51842098+shin1007@users.noreply.github.com>
2025-09-22 06:51:57 +0900

    検索結果の判定機能を追加。コース一覧が放送大学のコースと間違えやすいので改名

--- ouj-chromium-extension/src/thumbnail-progress.js
+++ ouj-chromium-extension/src/thumbnail-progress.js
@@ -155,6 +155,8 @@ function initializeThumbnailProgress() {
 
 // ページ読み込み時に初期化
 // TODO: 現状だとどのページでも対応させている形なので、一部のページのみに絞る
+// 検索ページ：https://v.ouj.ac.jp/view/ouj/#/navi/vod?se=%25E8%258B%25B1%25E8%25AA%259E
+
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', initializeThumbnailProgress);
 } else {
